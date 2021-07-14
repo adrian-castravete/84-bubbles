b6d29750c42819241e47b2e2ec2d5953f988c47c
Adrian Castravete <adrian.castravete@gmail.com>
2021-07-14 23:44:13 +0300

    Add basic sprite handling

--- things.lua
+++ things.lua
@@ -1,6 +1,8 @@
+local lg = love.graphics
+
 local Sprite = {
 	x = 160,
-	y = 100,
+	y = 120,
 	w = 16,
 	h = 16,
 	q = nil,
@@ -8,12 +10,43 @@ local Sprite = {
 }
 
 function Sprite:draw()
+	local c = self.color
+	local img = self.img
+	local w, h = self.w, self.h
+	local wo, ho = -w*0.5, -h*0.5
 
+	if not c then
+		c = {1, 1, 1}
+		if not img then
+			c = {math.random(), math.random(), math.random()}
+		end
+	end
+
+	lg.push()
+	lg.translate(self.x, self.y)
+	lg.setColor(c)
+	if img then
+		local q = self.q
+		if q then
+			lg.draw(img, q, wo, ho)
+		else
+			lg.draw(img, wo, ho)
+		end
+	else
+		lg.rectangle("fill", wo, ho, w, h)
+	end
+	lg.setColor(1, 1, 1)
+	lg.pop()
 end
 
-local Hero = {
+local Hero = Age.clone(Sprite, {
 	x = 160,
 	y = 120,
-}
+	color = {1, 0.5, 0},
+})
+
+function Hero:update(dt)
+	self:draw()
+end
 
 Age.template("hero", Hero)
