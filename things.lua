b8a60b268984827f52df505d726796017d0eeeba
Adrian Castravete <adrian.castravete@gmail.com>
2021-07-21 17:53:45 +0300

    Add bubble bullet

--- things.lua
+++ things.lua
@@ -1,4 +1,5 @@
 local lg = love.graphics
+local img = lg.newImage("slimo.png")
 
 age.component("sprite", {
 	x = 160,
@@ -7,6 +8,34 @@ age.component("sprite", {
 	h = 16,
 	q = nil,
 	img = nil,
+})
+
+age.component("hero", {
+	parents = {"sprite"},
+	color = {1, 0.5, 0},
+	state = "idle",
+})
+
+local bubbleQuads = {
+	0, 0, 0, 0,
+	0, 1, 1, 2,
+	2, 2, 1, 1,
+	2, 1, 1, 1,
+}
+for i=1, #bubbleQuads do
+	bubbleQuads[i] = lg.newQuad(bubbleQuads[i] * 8, 48, 8, 8, 128, 128)
+end
+age.component("hero-bubble", {
+	parents = {"sprite"},
+	img = img,
+	q = bubbleQuads[1],
+	w = 8,
+	h = 8,
+	headingLeft = false,
+	speed = 2,
+	animSpeed = 0.0625,
+	step = 1,
+	accumStep = 0,
 })
 
 age.system("sprite", function (e)
@@ -28,9 +57,9 @@ age.system("sprite", function (e)
 	if img then
 		local q = e.q
 		if q then
-			lg.draw(img, q, wo, ho)
+			lg.draw(img, q, wo, ho, 0)
 		else
-			lg.draw(img, wo, ho)
+			lg.draw(img, wo, ho, 0)
 		end
 	else
 		lg.rectangle("fill", wo, ho, w, h)
@@ -39,15 +68,28 @@ age.system("sprite", function (e)
 	lg.pop()
 end)
 
-age.component("hero", {
-	parents = {"sprite"},
-	color = {1, 0.5, 0},
-})
+age.system("hero-bubble", function (e, dt)
+	e.accumStep = e.accumStep + dt
+	if e.accumStep > e.animSpeed then
+		e.accumStep = e.accumStep - e.animSpeed
+		e.step = e.step + 1
+		e.x = e.x + e.speed
+		if e.step > #bubbleQuads then
+			e.destroy = true
+			return
+		end
+		e.q = bubbleQuads[e.step]
+	end
+end)
 
 age.receive("hero", "keypressed", function (e, key)
-	print("pressed " .. key)
+	if key == "space" then
+		age.entity("hero-bubble", {
+			x = e.x + 8,
+			y = e.y,
+		})
+	end
 end)
 
 age.receive("hero", "keyreleased", function (e, key)
-	print("released " .. key)
 end)
